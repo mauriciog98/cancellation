7975d66af56909c667a292e6e3065de6dd54e248
Andres Mauricio <programador@lcnidiomas.com>
2022-06-13 15:31:01 -0500

    change on permission

--- resources/js/Jetstream/AuthenticationCard.vue
+++ resources/js/Jetstream/AuthenticationCard.vue
@@ -2,7 +2,7 @@
     <div class="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50">
         <div class="absolute inset-0 bg-orange-500 bg-center"></div>
         <div
-            class="px-z relative left-0 top-0 h-screen w-full max-w-lg rounded-r-[4rem] bg-white pt-10 pb-8 sm:max-w-2xl sm:pr-10 lg:rounded-r-[6rem]">
+            class="px-z relative left-0 top-0 h-screen w-full max-w-lg rounded-r-lg bg-white pt-10 pb-8 sm:max-w-2xl sm:pr-10 lg:rounded-r-[6rem]">
             <div class="text mx-auto max-w-sm">
                 <div class="text-center">
                     <slot name="logo"/>
